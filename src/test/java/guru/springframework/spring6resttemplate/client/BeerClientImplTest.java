c84e88ada926c3737466f46ba1415d44137b2790
Rob van Dijk <rob@optimust.nl>
2024-02-05 22:41:49 +0100

    URL parameters

--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,14 +1,29 @@
 package guru.springframework.spring6resttemplate.client;
 
+import guru.springframework.spring6resttemplate.model.BeerDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class BeerClientImplTest {
 
     @Autowired
     BeerClientImpl beerClient;
+
+    @Test
+    void listBeerById() {
+
+        Page<BeerDTO> beerDTOS = beerClient.listBeers();
+        BeerDTO dto = beerDTOS.getContent().get(0);
+
+        BeerDTO byId = beerClient.getBeerById(dto.getId());
+
+        assertThat(byId).isNotNull();
+    }
 
     @Test
     void listBeersNoName() {
