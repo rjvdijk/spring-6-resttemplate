5b17bdc9d770ed850227e0a60b5198e8c601a3b1
Rob van Dijk <rob@optimust.nl>
2024-02-05 17:08:21 +0100

    Get List as String

--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,12 +1,19 @@
 package guru.springframework.spring6resttemplate.client;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class BeerClientImplTest {
+
+    @Autowired
+    BeerClientImpl beerClient;
 
     @Test
     void listBeers() {
 
+        beerClient.listBeers();
     }
 
 }
